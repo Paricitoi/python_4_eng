ac5f106c29619f803f3bd57cd8c48e47140b987c
Paricitoi <paricitoi@gmail.com>
2017-01-28 19:41:00 -0800

    week2

---
+++ python_week2/week2_ex1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+'''this comment'''
+import my_func
+
+def main():
+	my_func()
+
+if __name__ == "__main__":
+	main()
+
---
+++ python_week2/week2_ex2a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+'''comment number 2'''
+import telnetlib
+import sys
+import time
+import getpass
+
+TELNET_PORT = 23
+TELNET_TIMEOUT = 6
+
+def send_command(remote_conn, cmd):
+	time.sleep(1)
+	cmd = cmd.rstrip()
+	remote_conn.write(cmd + '\n')
+	time.sleep(1)
+	return remote_conn.read_very_eager()
+
+def login(remote_conn, username, password):
+        output = remote_conn.read_until("sername:", TELNET_TIMEOUT)
+        remote_conn.write(username + '\n')
+        output += remote_conn.read_until("ssword:", TELNET_TIMEOUT)
+        remote_conn.write(password + '\n')
+	return output
+
+
